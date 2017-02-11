d1fa49f3cde2062f154b48dfda185ec1dc1a271e
Erick Salas-Chaverri <ericksc@gmail.com>
2017-02-11 16:30:44 -0600

    Adding function for DB conection

---
+++ code/engine/query_creator.php
@@ -0,0 +1,20 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function ReadCompanyParams() { print "en update patient" ;   }
+function ReadPatientParams() {  print "en update patient" ;  }
+function DeletePatientParams() {  }
+function DeleteCompanyParams() {  }
+function InsertPatientParams() {  }
+function InsertCompanyParams() {  }
+function UpdatePatientParams() {  }
+function UpdateCompanyParams() {  print "en update patient" ;}
+function LoginUserParams() {      }
+
+?>
+
