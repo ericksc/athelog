295268cc57778a7a33ecd9c9afcbb03c026c6bf3
RamVQ <rvq25@hotmail.com>
2017-03-06 06:25:41 -0600

    logging added

--- code/engine/session.php
+++ code/engine/session.php
@@ -1,23 +1,24 @@
 <?php
 
 include("DBArrayQuery.php");
-include ("conexionDB.php");
+include_once "conexionDB.php";
 
 //store roles 
 $UserGroupAsInt = array(
     "NONE" => 0,
-    "Guest" => 1,
-    "Viewer" => 2,
-    "Editor" => 3,
-    "Administrator"=>4,
-    "Owner"=>5,
-    "Developer"=>6,
-    "Technician"=>-1,
+    "guest" => 1,
+    "viewer" => 2,
+    "editor" => 3,
+    "administrator"=>4,
+    "owner"=>5,
+    "developer"=>6,
+    "technician"=>-1,
 ); 
 
 $array = array(
     "foo" => "bar",
     "bar" => "foo",
+    "ojo" => 135,
     100   => -100,
     -100  => 100,
 );
@@ -55,19 +56,21 @@ function SessionDestroy(){
 //returns array = [UserGroup as String][as Integer]
 function ReadUserGroup($userid){
     
+    global $UserGroupAsInt;
     $result = "NONE";
     $query = "SELECT UserGroup FROM Users WHERE";
     $query.= " UserID='".$userid."'";
     //echo "<br>query=".$query;
     $rawdB_result=ConexionDB_rawdata($query);//result of query
     //echo "<br>Resultado Raw=".print_r($rawdB_result);//full result
-    $string_res = $rawdB_result[0]['UserGroup'];
-    $int_res = $UserGroupAsInt[$string_res];
-    echo "<br>int res=".$UserGroupAsInt['Editor'];
+    $string_res = $rawdB_result[0]['UserGroup']; //echo "<br>Executing ReadUserGroup, string res=".$string_res;
+    $int_res = $UserGroupAsInt[$string_res]; //echo "<br>Executing ReadUserGroup, int_res=".$int_res;
+  
+    //echo "<br>int res=".$UserGroupAsInt["Editor"];
     
     $result=array(            
         "StringUG"=> $string_res,
-        "IntUG" => $int_res
+        "IntUG" => $int_res,
             
     );
     
@@ -76,16 +79,17 @@ function ReadUserGroup($userid){
 }
 //eof
 
-
 //END OF FUNCTION DEF
 
 //testing
-print_r ($array);
-echo "<br>";
-echo "<br>usergroupasint element=".$UserGroupAsInt[0][$string_res];
-echo "<br>";print_r ($UserGroupAsInt);
-print_r (ReadUserGroup("111610819"));
-
+//print_r ($array);
+//$j='ojo';
+//echo "<br>array element=".$array[$j];
+//echo "<br>usergroupasint element=".$UserGroupAsInt['Editor'];
+echo "<br>usergroupasint elementx2=".$UserGroupAsInt['Editor'];
+//echo "<br>";print_r ($UserGroupAsInt);
+//echo "<br>result of lookup=";
+//print_r (ReadUserGroup("111610820"));
 
 
 
