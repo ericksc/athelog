dd8ae02bd58276e7732e98900e9b21780427925d
RamVQ <rvq25@hotmail.com>
2017-03-08 01:43:32 -0600

    notifications

--- code/engine/Notifications_Mail.php
+++ code/engine/Notifications_Mail.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once "ReadXML.php";
+include "ReadXML.php";
 
 
 //VAR DEF
@@ -33,28 +33,51 @@ $CompanyProfileDeletedMessage=[];$CompanyProfileDeletedReceivers=[]
 
 function SendMailNotification_UserProfileCreate($target_userid, $modifier){
     
+    echo "<br><br>Executing SendMailNotification_UserProfileCreate";
+    
     global $UserProfileCreatedReceivers;
+    global $TechnicianMailAccounts;
+    global $DeveloperMailAccounts;
+    global $OwnerMailAccounts;
+    
     $CC_Receivers="";
     $BC_Receivers="";
+    $TO_Receivers=$target_userid;
+    
+    echo "<br>UserProfileCreatedReceivers="; print_r ($UserProfileCreatedReceivers);
+    echo "<br>TechnicianMailAccounts=".$TechnicianMailAccounts;
     
     foreach($UserProfileCreatedReceivers as $key=>$val) { 
-            
-        if(strcmp($val,"CC")==0){
-            $CC_Receivers.=$key;
-        }        
         
+        echo "<br>Checking settings for $key";
+        
+        if(strcmp($key,"Technician")==0){
+            
+            if(strcmp($val,"CC")==0)$CC_Receivers.=$TechnicianMailAccounts;
+            else if(strcmp($val,"BC")==0)$BC_Receivers.=$TechnicianMailAccounts;
+            else if(strcmp($val,"TO")==0)$TO_Receivers.=$TechnicianMailAccounts.",";
+            
+        }else if(strcmp($key,"Developer")==0){
+
+            if(strcmp($val,"CC")==0)$CC_Receivers.=$DeveloperMailAccounts;
+            else if(strcmp($val,"BC")==0)$BC_Receivers.=$DeveloperMailAccounts;
+            else if(strcmp($val,"TO")==0)$TO_Receivers.=$DeveloperMailAccounts.",";            
+            
+        }else if(strcmp($key,"Owner")==0){
+            
+            if(strcmp($val,"CC")==0)$CC_Receivers.=$OwnerMailAccounts;
+            else if(strcmp($val,"BC")==0)$BC_Receivers.=$OwnerMailAccounts;
+            else if(strcmp($val,"TO")==0)$TO_Receivers.=$OwnerMailAccounts.","; 
+        }
+        
+
     } 
     //end of for
     
-    foreach($UserProfileDataChangedReceivers as $key=>$val) { 
-            
-        if(strcmp($val,"CC")==0){
-            $CC_Receivers.=$key;
-        }        
-        
-    } 
-    //end of for
-
+    //echo "<br><br>SendMailNotification_UserProfileCreate:";
+   // echo "<br>CC_Receivers=.$CC_Receivers";
+    //echo "<br>BC_Receivers=".$BC_Receivers;
+    //echo "<br>TO_Receivers=".$TO_Receivers;
     
     
     //$MailDestination=
@@ -69,7 +92,7 @@ function SendMailNotification_UserProfileCreate($target_userid, $modifier){
 
 //END OF FUNCT DEF
 
-
+SendMailNotification_UserProfileCreate("aa","gg");
 
 
 ?>
