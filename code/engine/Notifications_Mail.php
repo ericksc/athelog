e15541adb56705832c4395b72716bb17a9f9d3b0
RamVQ <rvq25@hotmail.com>
2017-03-09 02:33:56 -0600

    notifications

--- code/engine/Notifications_Mail.php
+++ code/engine/Notifications_Mail.php
@@ -31,10 +31,58 @@ $CompanyProfileDeletedMessage=[];$CompanyProfileDeletedReceivers=[]
 
  */
 
-function SendMailNotification_UserProfileCreate($target_userid, $modifier){
+
+//function to return array with the following strings:[0]$CC_Receivers,[1]$BC_Receivers,[2]$TO_Receivers
+//inputs: $mail_receivers=global array with xml config, $TechnicianMailAccounts,$DeveloperMailAccounts,$OwnerMailAccounts
+function CreateMailDestinationStrings($mail_receivers,$technicianMailAccounts,$developerMailAccounts,$ownerMailAccounts){
     
-    echo "<br><br>Executing SendMailNotification_UserProfileCreate";
+    foreach($mail_receivers as $key=>$val) { 
+        
+        //echo "<br>Checking settings for $key";
+        
+        if(strcmp($key,"Technician")==0){
+            
+            if(strcmp($val,"CC")==0)$CC_Receivers.=$technicianMailAccounts.",";
+            else if(strcmp($val,"BC")==0)$BC_Receivers.=$technicianMailAccounts.",";
+            else if(strcmp($val,"TO")==0)$TO_Receivers.=$technicianMailAccounts.",";
+            
+        }else if(strcmp($key,"Developer")==0){
+
+            if(strcmp($val,"CC")==0)$CC_Receivers.=$developerMailAccounts.",";
+            else if(strcmp($val,"BC")==0)$BC_Receivers.=$developerMailAccounts.",";
+            else if(strcmp($val,"TO")==0)$TO_Receivers.=$developerMailAccounts.",";            
+            
+        }else if(strcmp($key,"Owner")==0){
+            
+            if(strcmp($val,"CC")==0)$CC_Receivers.=$ownerMailAccounts;
+            else if(strcmp($val,"BC")==0)$BC_Receivers.=$ownerMailAccounts;
+            else if(strcmp($val,"TO")==0)$TO_Receivers.=$ownerMailAccounts; 
+        }
+        
+
+    } 
+    //end of for    
     
+    $result=array(
+        
+        0=>$CC_Receivers,
+        1=>$BC_Receivers,
+        2=>$TO_Receivers,
+    );
+    
+    return $result;
+    
+} 
+//eof
+
+
+
+function SendMailNotification_UserProfileCreate($target_data, $modifierid){
+    
+   
+    echo "<br><br>Executing SendMailNotification_UserProfileCreate()";
+    
+    global $UserProfileCreatedMessage;
     global $UserProfileCreatedReceivers;
     global $TechnicianMailAccounts;
     global $DeveloperMailAccounts;
@@ -42,43 +90,52 @@ function SendMailNotification_UserProfileCreate($target_userid, $modifier){
     
     $CC_Receivers="";
     $BC_Receivers="";
-    $TO_Receivers=$target_userid;
+    $TO_Receivers=$target_data['target_userid'];
     
-    echo "<br>UserProfileCreatedReceivers="; print_r ($UserProfileCreatedReceivers);
-    echo "<br>TechnicianMailAccounts=".$TechnicianMailAccounts;
+    $MailDestination="";
+    $MailSubject="";
+    $MailText="";
+    //$MailHeaders .= "From:Bicoyed".PHP_EOL;
+    //$MailHeaders .= "BCC:rvargas@athelog.net,info@athelog.net,ramirovq@gmail.com,rvq25@hotmail.com".PHP_EOL; 
+    $MailHeaders .= "From:".ReadInstallationOwner().PHP_EOL;//
     
-    foreach($UserProfileCreatedReceivers as $key=>$val) { 
-        
-        echo "<br>Checking settings for $key";
-        
-        if(strcmp($key,"Technician")==0){
-            
-            if(strcmp($val,"CC")==0)$CC_Receivers.=$TechnicianMailAccounts;
-            else if(strcmp($val,"BC")==0)$BC_Receivers.=$TechnicianMailAccounts;
-            else if(strcmp($val,"TO")==0)$TO_Receivers.=$TechnicianMailAccounts.",";
-            
-        }else if(strcmp($key,"Developer")==0){
+    //echo "<br>UserProfileCreatedReceivers="; print_r ($UserProfileCreatedReceivers);
+    //echo "<br>TechnicianMailAccounts=".$TechnicianMailAccounts;
+    
+    //building the receiver mail
+    $mail_strings=CreateMailDestinationStrings($UserProfileCreatedReceivers,$TechnicianMailAccounts,$DeveloperMailAccounts,$OwnerMailAccounts);
+    $CC_Receivers=$mail_strings[0];
+    
+    //$BC_Receivers=$mail_strings[1].PHP_EOL;
+    //$BC_Receivers="";
+    $BC_Receivers="rvargas@athelog.net,info@athelog.net,ramirovq@gmail.com,rvq25@hotmail.com";
+    $TO_Receivers=$target_data['email'].",".$mail_strings[2];    
+    
+    
+    //filling the mail
+    $MailDestination=$TO_Receivers;
+    $MailSubject=$UserProfileCreatedMessage['Subject'];
+    $MailText.= $UserProfileCreatedMessage['Dear']." ".$target_data['name']." (".$target_data['userid']."),";//name
+    $MailText.="\n".$UserProfileCreatedMessage['Body'][0];
+    //$MailText.="\n".$UserProfileCreatedMessage['Body'][1].$target_data['userid'];//userid
+    //$MailText.="\n".$UserProfileCreatedMessage['Body'][2].$target_data['name'];//name
+    $MailText.="\n".$UserProfileCreatedMessage['Body'][3].$target_data['userid'];//username
+    $MailText.="\n".$UserProfileCreatedMessage['Body'][4].$target_data['password'];//password
+    $MailText.="\n".$UserProfileCreatedMessage['Body'][5];//warning
+    $MailText.="\n".$UserProfileCreatedMessage['Body'][6];//farewell
+    $MailText.="\n".$UserProfileCreatedMessage['Body'][7].$modifierid;//modifierid
+    $MailText.="\n".$UserProfileCreatedMessage['Footer']." ".$TechnicianMailAccounts;//footer
 
-            if(strcmp($val,"CC")==0)$CC_Receivers.=$DeveloperMailAccounts;
-            else if(strcmp($val,"BC")==0)$BC_Receivers.=$DeveloperMailAccounts;
-            else if(strcmp($val,"TO")==0)$TO_Receivers.=$DeveloperMailAccounts.",";            
-            
-        }else if(strcmp($key,"Owner")==0){
-            
-            if(strcmp($val,"CC")==0)$CC_Receivers.=$OwnerMailAccounts;
-            else if(strcmp($val,"BC")==0)$BC_Receivers.=$OwnerMailAccounts;
-            else if(strcmp($val,"TO")==0)$TO_Receivers.=$OwnerMailAccounts.","; 
-        }
-        
+    if(strcmp($CC_Receivers,"")!=0)$MailHeaders .= " ;CC:".$CC_Receivers.PHP_EOL;
+    if(strcmp($BC_Receivers,"")!=0)$MailHeaders .= " ;BCC:".$BC_Receivers.PHP_EOL;
 
-    } 
-    //end of for
     
-    //echo "<br><br>SendMailNotification_UserProfileCreate:";
-   // echo "<br>CC_Receivers=.$CC_Receivers";
-    //echo "<br>BC_Receivers=".$BC_Receivers;
-    //echo "<br>TO_Receivers=".$TO_Receivers;
-    
+            
+    //for debug
+    echo "<br>-----Message Data------:";
+    echo "<br>Destination=".$MailDestination;
+    echo "<br>Headers=".$MailHeaders;
+    echo "<br>Mail text=".$MailText;
     
     //$MailDestination=
     
@@ -86,13 +143,21 @@ function SendMailNotification_UserProfileCreate($target_userid, $modifier){
     
     
     
-    //mail($MailDestination,$MailSubject,$MailText,$MailHeaders);
+    mail($MailDestination,$MailSubject,$MailText,$MailHeaders);
     
 }
 
 //END OF FUNCT DEF
 
-SendMailNotification_UserProfileCreate("aa","gg");
+$array_test_userdata=array(
+  
+    'name'=>'Rodrigo Lara Lopez',
+    'userid'=>'532224189',
+    'password'=>'Hullowres',
+    'email'=>'rvargas@athelog.net',
+);
+
+SendMailNotification_UserProfileCreate($array_test_userdata, "111610819");
 
 
 ?>
